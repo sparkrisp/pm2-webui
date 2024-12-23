2b849232852a49439ba7ff6f0f64622cdc999ef2
Ariel Ivan Ruiz Mateos <arielrm@gmail.com>
2024-12-23 15:34:55 -0300

    Update api.js npm install added

--- src/providers/pm2/api.js
+++ src/providers/pm2/api.js
@@ -1,5 +1,6 @@
 const pm2 = require('pm2');
-const { bytesToSize, timeSince } = require('./ux.helper')
+const { exec } = require('child_process');
+const { bytesToSize, timeSince } = require('./ux.helper');
 
 function listApps(){
     return new Promise((resolve, reject) => {
@@ -46,7 +47,7 @@ function describeApp(appName){
                         cpu: apps[0].monit.cpu,
                         memory: bytesToSize(apps[0].monit.memory),
                         uptime: timeSince(apps[0].pm2_env.pm_uptime),
-                        pm_id: apps[0].pm_id, 
+                        pm_id: apps[0].pm_id,
                         pm_out_log_path: apps[0].pm2_env.pm_out_log_path,
                         pm_err_log_path: apps[0].pm2_env.pm_err_log_path,
                         pm2_env_cwd: apps[0].pm2_env.pm_cwd
@@ -59,6 +60,50 @@ function describeApp(appName){
             })
         })
     })
+}
+
+function npmInstallAndRestart(appName) {
+    return new Promise((resolve, reject) => {
+        // Primero obtenemos la información de la aplicación para saber su directorio
+        describeApp(appName)
+            .then(app => {
+                if (!app) {
+                    reject(new Error(`Application ${appName} not found`));
+                    return;
+                }
+
+                const workDir = app.pm2_env_cwd;
+                
+                // Ejecutamos npm install en el directorio de la aplicación
+                exec('npm install', { cwd: workDir }, (error, stdout, stderr) => {
+                    if (error) {
+                        reject(new Error(`npm install failed: ${error.message}\n${stderr}`));
+                        return;
+                    }
+
+                    // Una vez completado npm install, reiniciamos la aplicación
+                    pm2.connect((err) => {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+
+                        pm2.restart(appName, (err, proc) => {
+                            pm2.disconnect();
+                            if (err) {
+                                reject(err);
+                                return;
+                            }
+                            resolve({
+                                npmOutput: stdout,
+                                process: proc
+                            });
+                        });
+                    });
+                });
+            })
+            .catch(reject);
+    });
 }
 
 function reloadApp(process){
@@ -117,6 +162,6 @@ module.exports = {
     describeApp,
     reloadApp,
     stopApp,
-    restartApp
+    restartApp,
+    npmInstallAndRestart
 }
-
